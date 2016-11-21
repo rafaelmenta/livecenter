30b8c20be5568be2461f809a4e22de8d0d3816a1
Rafael Guedes <v-gueder-ave@ae.com>
2016-11-21 14:14:11 -0800

    Add storage expiration

--- app/scripts/service/storage.js
+++ app/scripts/service/storage.js
@@ -2,19 +2,40 @@ angular.module('livecenter').service('Storage', function($window) {
   
   var localStorage = $window.localStorage;
 
+  // private
+
+  var getTodayTimestamp = function() {
+  	var today = new Date();
+  	return today.getFullYear() + '-' + today.getMonth() + '-' + today.getDate();
+  }
+
+  var updateTimestamp = function() {
+  	var timestamp = getTodayTimestamp();
+  	localStorage.setItem('timestamp', timestamp);
+  }
+
+  var isExpired = function() {
+  	var today = getTodayTimestamp();
+  	var timestamp = localStorage.getItem('timestamp');
+  	return today !== timestamp;
+  }
+
   var isStorageAvailable = function() {
   	return localStorage && localStorage.setItem && localStorage.getItem;
   };
+
+  // public
 
   var setItem = function(key, item) {
   	if (!isStorageAvailable) return false;
 
   	localStorage.setItem(key, item);
+  	updateTimestamp();
   	return true;
   }
 
   var getItem = function(key) {
-  	if (!isStorageAvailable) return null;
+  	if (!isStorageAvailable || isExpired()) return null;
 
   	return localStorage.getItem(key);
   };
