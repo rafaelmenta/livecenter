1ac11039b98674b716095d25fb63a4ceffab0846
Rafael Guedes <rafaelmenta@gmail.com>
2016-12-07 00:02:37 -0800

    Add last commit hash to assets names

--- gulp/task/build.js
+++ gulp/task/build.js
@@ -3,6 +3,7 @@ var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var htmlreplace = require('gulp-html-replace');
+var git = require('git-rev');
 
 module.exports = function(gulp, options) {
   var path = options.distPath;
@@ -11,32 +12,42 @@ module.exports = function(gulp, options) {
 
   return function() {
 
+  git.short(function(hash) {
+
+    var scriptsFilename = `scripts-${hash}.min.js`,
+        vendorsFilename = `vendors-${hash}.js`,
+        cssFilename = `styles-${hash}.css`,
+        jsPath = `${path}/js`
+
     gulp.src(`${appPath}/scripts/**/*.js`)
-      .pipe(concat('scripts.js'))
-      .pipe(gulp.dest(`${path}/js`))
-      .pipe(rename('scripts.min.js'))
+      .pipe(concat(`scripts-${hash}.js`))
+      .pipe(gulp.dest(jsPath))
+      .pipe(rename(scriptsFilename))
       .pipe(uglify())
-      .pipe(gulp.dest(`${path}/js`));
+      .pipe(gulp.dest(jsPath));
 
     gulp.src(vendors)
-      .pipe(concat('vendors.js'))
-      .pipe(gulp.dest(`${path}/js`));
+      .pipe(concat(vendorsFilename))
+      .pipe(gulp.dest(jsPath));
 
     gulp.src(`${appPath}/views/**/*.html`)
       .pipe(gulp.dest(`${path}/views/`));
 
     gulp.src(`${appPath}/index.html`)
       .pipe(htmlreplace({
-        js : `js/scripts.min.js`,
+        js : `js/${scriptsFilename}`,
+        css : `css/${cssFilename}`,
         vendor : {
           src: null,
-          tpl: '<script src="js/vendors.js"></script>'
+          tpl: `<script src="js/${vendorsFilename}"></script>`
         }
       }))
       .pipe(gulp.dest(`${path}/`));
 
     gulp.src(`${appPath}/css/**/*.css`)
       .pipe(cleanCSS())
+      .pipe(rename(cssFilename))
       .pipe(gulp.dest(`${path}/css/`));
+  });
   }
 };
