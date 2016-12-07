c3b3a28c72a99661a5dc9b4817ca2c884f2bccec
Rafael Guedes <rafaelmenta@gmail.com>
2016-12-06 23:08:39 -0800

    Create build task

---
+++ gulp/task/build.js
@@ -0,0 +1,43 @@
+var cleanCSS = require('gulp-clean-css');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var htmlreplace = require('gulp-html-replace');
+
+var vendors = [
+  './node_modules/angular/angular.min.js',
+  './node_modules/angular-ui-router/release/angular-ui-router.min.js'
+];
+
+module.exports = function(gulp, path) {
+  return function() {
+
+    gulp.src('./app/scripts/**/*.js')
+      .pipe(concat('scripts.js'))
+      .pipe(gulp.dest(`${path}/js`))
+      .pipe(rename('scripts.min.js'))
+      // .pipe(uglify())
+      .pipe(gulp.dest(`${path}/js`));
+
+    gulp.src(vendors)
+      .pipe(concat('vendors.js'))
+      .pipe(gulp.dest(`${path}/js`));
+
+    gulp.src('./app/views/**/*.html')
+      .pipe(gulp.dest(`${path}/views/`));
+
+    gulp.src('./app/index.html')
+      .pipe(htmlreplace({
+        js : `js/scripts.min.js`,
+        vendor : {
+          src: null,
+          tpl: '<script src="js/vendors.js"></script>'
+        }
+      }))
+      .pipe(gulp.dest(`${path}/`));
+
+    gulp.src('./app/css/**/*.css')
+      .pipe(cleanCSS())
+      .pipe(gulp.dest(`${path}/css/`));
+  }
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+const distPath = './dist';
+
 var gulp = require('gulp');
 
 var data = require('gulp-data');
@@ -6,8 +8,20 @@ var stylus = require('gulp-stylus');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 
+
 var stylusTask = require('./gulp/task/stylus')(gulp, stylus, data, reload);
 var defaultTask = require('./gulp/task/default')(gulp, browserSync, reload);
+var buildTask = require('./gulp/task/build')(gulp, distPath );
+var cleanTask = require('./gulp/task/clean')(gulp, distPath);
 
-gulp.task('stylus', stylusTask)
+gulp.task('stylus', stylusTask);
+gulp.task('clean', cleanTask);
 gulp.task('default', ['stylus'], defaultTask);
+gulp.task('build', ['clean'], buildTask);
+gulp.task('build-server', function() {
+  browserSync({
+    server : {
+      baseDir : 'dist'
+    }
+  })
+});
