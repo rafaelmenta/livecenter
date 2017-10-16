e1d161861f38ce819b6b257bb7d15f53afba5023
Rafael Guedes <rafaelmenta@gmail.com>
2017-10-15 22:53:14 -0700

    Update URL

--- app/scripts/config/constant.js
+++ app/scripts/config/constant.js
@@ -1,6 +1,6 @@
 app.constant('API', {
-  GAMES : 'http://www.draftbrasil.net/superliga/bdb/get_date_games',
-  BOX : 'http://api.draftbrasil.net/nba/boxscore/'
+  GAMES : 'http://www.draftbrasil.org/superliga/bdb/get_date_games',
+  BOX : 'http://api.draftbrasil.org/nba/boxscore/'
 });
 
 app.constant('DEFAULT_SETTINGS', {
